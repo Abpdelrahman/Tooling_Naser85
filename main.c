91fe6408a8167298dd84f7f42ae76628f0a293f2
Abdelrahman  Ibrahim <abdibrahim@stu.okan.edu.tr>
2024-09-23 02:51:36 +0300

    v1.0.0

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Hamada 10
+
+
+int main()
+
+{
+
+int x = Hamada;
+int y = 15 , z = 30 ;
+
+printf ("X value : %i\n", x);
+printf ("Y value : %i\n", y);
+printf ("Z value : %i\n", z);
+
+return 0 ;
+
+}
